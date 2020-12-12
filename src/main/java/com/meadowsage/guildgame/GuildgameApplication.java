95583d8c64c65db3beda365322d4c0df82c9cb60
Tomoyuki Uehara <mjht2236@gmail.com>
2020-12-12 22:01:52 +0900

    initial commit

---
+++ src/main/java/com/meadowsage/guildgame/GuildgameApplication.java
@@ -0,0 +1,13 @@
+package com.meadowsage.guildgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meadowsage/guildgame/GuildgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meadowsage.guildgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
